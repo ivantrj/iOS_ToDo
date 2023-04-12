e4b3a2618f981d08ccf100b3f0665cfd0535e90a
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-04-12 13:55:29 +0200

    first commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Ivan Trajanovski on 12.04.23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
